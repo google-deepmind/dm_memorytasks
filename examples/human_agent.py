49592e154062ab087792bd9a5124d2543f6dcef4
Ricardo Barreira <rsf@google.com>
2019-12-10 05:22:08 -0800

    Disable pygame mixer module in the human_agent example, in order to decrease CPU usage.
    PiperOrigin-RevId: 284748591
    Change-Id: I23c53658757ef86b70883719a5b6a61d83e42bb5

--- examples/human_agent.py
+++ examples/human_agent.py
@@ -59,6 +59,7 @@ _NO_ACTION = {
 
 def main(_):
   pygame.init()
+  pygame.mixer.quit()
   pygame.display.set_caption('Memory Tasks Human Agent')
 
   env_settings = dm_memorytasks.EnvironmentSettings(
