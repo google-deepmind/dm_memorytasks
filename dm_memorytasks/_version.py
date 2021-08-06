d03cbf11715eeb672353c0b88477911f05d73ed1
Tom Ward <tomward@google.com>
2021-08-06 08:05:51 -0700

    Set version to v1.0.3
    PiperOrigin-RevId: 389177087
    Change-Id: I184b6c4e373053ad3ce130c841831a01b2c7c1fe

--- dm_memorytasks/_version.py
+++ dm_memorytasks/_version.py
@@ -17,4 +17,4 @@
 Kept in separate file so it can be used during installation.
 """
 
-__version__ = '1.0.2'  # https://www.python.org/dev/peps/pep-0440/
+__version__ = '1.0.3'  # https://www.python.org/dev/peps/pep-0440/
